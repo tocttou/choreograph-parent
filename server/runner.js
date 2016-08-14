1405bf7c16224dd62935125bbfafe1c687fb2137
Ashish <me@ashishchaudhary.in>
2016-08-14 14:01:42 +0530

    several changes

--- server/runner.js
+++ server/runner.js
@@ -1,31 +1,153 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('choreoworkers.sqlite3');
 const Scheduler = require('redis-scheduler');
-console.log(process.env.NODE_ENV);
 const scheduler = new Scheduler({ host: '0.0.0.0', port: 6004 });
+const fs = require('fs');
+const Docker = require('dockerode');
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+const mkdirp = require('mkdirp');
+const byline = require('byline');
 
-const runLoop = (task, key) => {
-  scheduler.schedule({ key, expire: 5000, handler: task }, (err) => {
+const runLoop = (task, key, service) => {
+  scheduler.schedule({ key, expire: service.trigger.repeat, handler: task }, (err) => {
     if (err) {
       console.error(err);
-    } else {
-      console.log('scheduled succesfully!');
     }
   });
 };
 
 const loop = (err, key) => {
-  console.log('run callback for keyword: ', key);
-
   scheduler.cancel({ key }, (err) => {
     if (err) {
       return false;
     }
-    runLoop(loop, key);
+    perform({
+      cb: runLoop,
+      key,
+      func: loop
+    });
   });
-
-  // TODO: Here should be your code
 };
+
+function perform(configObject) {
+  const key = configObject.key;
+  db.serialize(() => {
+    db.get('SELECT rowid AS id, jobname, config FROM workers WHERE jobname = ?', key.split('-')[0],
+      (err, row) => {
+        if (row) {
+          const doc = JSON.parse(row.config.replace(/'/g, '"'));
+          const service = doc[key.split('-')[1]];
+          const pathPrefix = `/tmp/jobfiles/${key.split('-')[0]}/${key.split('-')[1]}`;
+
+          let runnersh = '#! /bin/bash\n';
+
+          for (const execType of Object.keys(service.exec)) {
+            switch (execType) {
+            case 'bash':
+              for (const command of service.exec.bash) {
+                runnersh += `${command}\n`;
+              }
+              break;
+            case 'python2':
+              for (const filename of Object.keys(service.exec.python2)) {
+                runnersh += `python ${filename}\n`;
+              }
+              break;
+            case 'python3':
+              for (const filename of Object.keys(service.exec.python3)) {
+                runnersh += `python3 ${filename}\n`;
+              }
+              break;
+            case 'node':
+              for (const filename of Object.keys(service.exec.node)) {
+                runnersh += `node ${filename}\n`;
+              }
+              break;
+            }
+          }
+
+          fs.writeFileSync(`${pathPrefix}/runner.sh`,
+            runnersh, { mode: '775' }, (err) => {
+              if (err) {
+                console.log('cannot write runnersh to /tmp');
+              }
+            });
+
+          docker.createContainer({
+            Image: 'tocttou/choreograph-child',
+            Cmd: ['/bin/bash', 'runner.sh'],
+            NetworkMode: 'host',
+            Tty: true,
+            Binds: ['/tmp:/tmp'],
+            WorkingDir: `/tmp/jobfiles/${key.split('-')[0]}/${key.split('-')[1]}`
+          }, (err, container) => {
+            if (container) {
+
+              if (typeof service.trigger.max_time !== 'undefined') {
+                scheduler.schedule({ key: `${key.split('-')[0]}-${container.id}`,
+                  expire: service.trigger.max_time, handler: removeContainer });
+              }
+
+              console.log(`#$#$# Job: ${key.split('-')[0]} - Service: ${key.split('-')[1]} #$#$#`);
+
+              container.attach({ stream: true, stdout: true, stderr: true }, (err, stream) => {
+                byline(stream).on('data', (line) => {
+                  if (line.toString().trim() !== '') {
+                    process.stdout.write(`#=># ${line}\n`);
+                  }
+                });
+              });
+
+              container.start((err, data) => {
+                console.log(data);
+                if (err) {
+                  console.log('\nCannot create job container');
+                }
+              });
+            }
+          });
+
+          if (typeof configObject.cb !== 'undefined' && typeof configObject.func !== 'undefined') {
+            configObject.cb(configObject.func, key, service);
+          }
+        }
+      });
+  });
+}
+
+function removeContainer(err, key) {
+  const containerid = key.split('-')[1];
+  const container = docker.getContainer(containerid);
+  container.stop((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      container.remove((err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`#$#$# Removed container: ${containerid.slice(0, 12)} #$#$#`);
+        }
+      });
+    }
+  });
+}
+
+function initLoopRunner(err, key) {
+  const modifiedKey = `${key.split('-')[0]}-${key.split('-')[1]}`;
+  db.get('SELECT rowid AS id, jobname, config FROM workers WHERE jobname = ?', key.split('-')[0],
+    (err, row) => {
+      if (row) {
+        const doc = JSON.parse(row.config.replace(/'/g, '"'));
+        if (typeof doc[key.split('-')[1]].trigger.repeat === 'undefined') {
+          perform({ key: modifiedKey });
+        } else {
+          runLoop(loop, modifiedKey, doc[key.split('-')[1]]);
+        }
+      }
+    });
+}
+
 
 
 export function runner(jobname) {
@@ -34,7 +156,49 @@ export function runner(jobname) {
       (err, row) => {
         if (row) {
           const doc = JSON.parse(row.config.replace(/'/g, '"'));
-          runLoop(loop, 'loop');
+          for (const service of Object.keys(doc)) {
+            if (service !== 'job' && service !== 'nodes') {
+
+              mkdirp(`/tmp/jobfiles/${jobname}/${service}`, (err) => {
+                if (err) {
+                  console.log(`cannot create /tmp/jobfiles/${jobname}/${service}`);
+                } else {
+
+                  if (typeof doc[service].payload === 'object') {
+                    for (const filename of Object.keys(doc[service].payload)) {
+                      fs.writeFile(`/tmp/jobfiles/${jobname}/${service}/${filename}`,
+                        doc[service].payload[filename], (err) => {
+                          if (err) {
+                            console.log('cannot write payload files to /tmp');
+                          }
+                        });
+                    }
+                  }
+
+                  if (typeof doc[service].exec !== 'undefined') {
+                    for (const execType of Object.keys(doc[service].exec)) {
+                      if (execType !== 'bash') {
+                        if (typeof doc[service].exec[execType] === 'object') {
+                          for (const filename of Object.keys(doc[service].exec[execType])) {
+                            fs.writeFile(`/tmp/jobfiles/${jobname}/${service}/${filename}`,
+                              new Buffer(doc[service].exec[execType][filename], 'base64'), (err) => {
+                                if (err) {
+                                  console.log('cannot write exec files to /tmp');
+                                }
+                              });
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              });
+
+              scheduler.schedule({ key: `${jobname}-${service}-starter`,
+                expire: doc[service].trigger.start, handler: initLoopRunner });
+
+            }
+          }
         }
       });
   });
